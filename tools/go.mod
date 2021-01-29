941656ef61fe41500664f97cda3da6f43808ba0f
Koichi Shiraishi <zchee.io@gmail.com>
2021-01-29 22:47:08 +0900

    tools: add tools

---
+++ tools/go.mod
@@ -0,0 +1,5 @@
+module github.com/zchee/pkg/tools
+
+go 1.15
+
+require github.com/golangci/golangci-lint v1.36.0
---
+++ tools/tools.go
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// SPDX-FileCopyrightText: Copyright 2021 The pkg Authors
+
+// +build tools
+
+package tools
+
+import (
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+)
